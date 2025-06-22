c0087bfe96893332536fc18c6c464ba12a4f6650
Shantanu Khokrale <shantanukhokrale14@gmail.com>
2025-06-22 12:18:39 +0530

    Update dashboard_controller.rb

--- app/controllers/employee/dashboard_controller.rb
+++ app/controllers/employee/dashboard_controller.rb
@@ -3,12 +3,6 @@ class Employee::DashboardController < ApplicationController
   before_action :require_employee
 
   def index
-    # @today_menu = Menu.find_by(menu_date: Date.current, organization_id: current_user.organization_id)
-    # if @today_menu
-    #   @response = current_user.responses.find_or_initialize_by(response_date: Date.current, menu_id: @today_menu.id)
-    # else
-    #   @response = nil
-    # end
     @today_menu = Menu.find_by(menu_date: Date.current, organization_id: current_user.organization_id)
     @response = current_user.responses.find_or_initialize_by(response_date: Date.current, menu: @today_menu)
     @can_submit = @today_menu&.submission_open?
@@ -28,21 +22,6 @@ class Employee::DashboardController < ApplicationController
       flash.now[:alert] = "There was an error saving your response."
       render :index
     end
-    # @today_menu = Menu.find_by(menu_date: Date.current, organization_id: current_user.organization_id)
-
-    # unless @today_menu
-    #   redirect_to employee_dashboard_path, alert: "No menu available for today."
-    #   return
-    # end
-
-    # @response = current_user.responses.find_or_initialize_by(response_date: Date.current, menu_id: @today_menu.id)
-
-    # if @response.update(response_params)
-    #   redirect_to employee_dashboard_path, notice: "Your preferences have been recorded successfully."
-    # else
-    #   flash.now[:alert] = "There was an error saving your response."
-    #   render :index
-    # end
   end
 
   private
